1f642dc87b0ef359a99c55bbf403d3a681f1437d
StalkerK0t <vasyap8262@gmail.com>
2023-05-24 17:37:39 +0300

    Joy Movement

--- raspberry/joystick.py
+++ raspberry/joystick.py
@@ -1,16 +1,3 @@
-'''
-  A     B
-  |     |
-  |     |
-  E-----F
-     |
-     |
-  H-----G
-  |     |
-  |     |
-  D     C
-'''
-
 from pyPS4Controller.controller import Controller
 import serial
 from time import sleep
@@ -20,21 +7,18 @@ class MyController(Controller):
         super().__init__(self, **kwargs)
         self.L1_on = False
         self.R1_on = False
-        self.L2_on = False
-        self.R2_on = False
         self.ser = serial.Serial("/dev/ttyACM0", 57600)
 
-        self.names_def = {'A': 'A', 'B': 'B', 'C': 'C', 'D': 'D', 'E': 'E', 'F': 'F', 'G': 'G', 'H': 'H'}
-        self.default_pos = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0}
-        self.current_pos = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0}
-        self.restrictions = {'A': (-4096, 4096), 'B': (-4096, 4096), 'C': (-4096, 4096), 'D': (-4096, 4096),
-                             'E': (-4096, 4096), 'F': (-4096, 4096), 'G': (-4096, 4096), 'H': (-4096, 4096)}
-        self.dU = 100
 
-    def send(self, lit, sign):
-        if self.restrictions[lit][0] < self.current_pos[lit] + sign * self.dU < self.restrictions[lit][1]:
-            self.ser.write((self.names_def[lit] + str(sign * self.dU).rjust(4, '0')).encode())
-            self.current_pos[lit] += sign * self.dU
+        self.default_pos = {'A1': 0, 'A2': 0, 'A3': 0, 'A4': 0}
+        self.current_pos = {'A1': 0, 'A2': 0, 'A3': 0, 'A4': 0}
+        self.restrictions = {'A1': (-4096, 4096), 'A2': (-4096, 4096), 'A3': (-4096, 4096), 'A4': (-4096, 4096)}
+
+    def send(self, lit, sign, val = 100):
+        if lit[0] != 'A' or self.restrictions[lit][0] < self.current_pos[lit] + sign * val < self.restrictions[lit][1] :
+            self.ser.write((lit + str(sign * val).rjust(4, '0')).encode())
+            if lit[0] == 'A':
+                self.current_pos[lit] += sign * val
 
     def on_L1_press(self):
         self.L1_on = True
@@ -42,95 +26,63 @@ class MyController(Controller):
     def on_L1_release(self):
         self.L1_on = False
 
-    def on_L2_press(self, value):
-        self.L2_on = True
-
-    def on_L2_release(self):
-        self.L2_on = False
-
     def on_R1_press(self):
         self.R1_on = True
 
     def on_R1_release(self):
         self.R1_on = False
 
+    def on_L2_press(self, value):
+        if self.L1_on: sign = -1
+        else: sign = 1
+
+        # нужно вычислить коэффициент k
+        k = 1
+        self.send("M1", sign, value * k)
+        self.send("M3", sign, value * k)
+
+    def on_L2_release(self):
+        self.send("M1", 1, 0)
+        self.send("M3", 1, 0)
+
     def on_R2_press(self, value):
-        self.R2_on = True
+        if self.L1_on: sign = -1
+        else: sign = 1
+
+
+        # нужно вычислить коэффициент k
+        k = 1
+        self.send("M2", sign, value * k)
+        self.send("M4", sign, value * k)
 
     def on_R2_release(self):
-        self.R2_on = False
+        self.send("M2", 1, 0)
+        self.send("M4", 1, 0)
 
     def on_x_press(self):
-        if self.R1_on:
-            self.send('B', 1)
-        elif self.L1_on:
-            self.send('F', -1)
-        else:
-            self.send('F', -1)
-            self.send('B', -1)
+        self.send('A2', 1)
 
     def on_triangle_press(self):
-        if self.R1_on:
-            self.send('B', -1)
-        elif self.L1_on:
-            self.send('F', 1)
-        else:
-            self.send('F', 1)
-            self.send('B', 1)
+        self.send('A2', -1)
 
     def on_circle_press(self):
-        if self.R1_on:
-            self.send('C', 1)
-        elif self.L1_on:
-            self.send('G', -1)
-        else:
-            self.send('G', -1)
-            self.send('C', -1)
+        self.send('A4', 1)
 
     def on_square_press(self):
-        if self.R1_on:
-            self.send('C', -1)
-        elif self.L1_on:
-            self.send('G', 1)
-        else:
-            self.send('G', 1)
-            self.send('C', 1)
+        self.send('A4', -1)
 
     def on_up_arrow_press(self):
-        if self.R1_on:
-            self.send('A', -1)
-        elif self.L1_on:
-            self.send('E', 1)
-        else:
-            self.send('E', 1)
-            self.send('A', 1)
+        self.send('A1', -1)
 
     def on_down_arrow_press(self):
-        if self.R1_on:
-            self.send('A', 1)
-        elif self.L1_on:
-            self.send('E', -1)
-        else:
-            self.send('E', -1)
-            self.send('A', -1)
+        self.send('A1', 1)
 
     def on_left_arrow_press(self):
-        if self.R1_on:
-            self.send('D', -1)
-        elif self.L1_on:
-            self.send('H', 1)
-        else:
-            self.send('H', 1)
-            self.send('D', 1)
+        self.send('A3', -1)
 
     def on_right_arrow_press(self):
-        if self.R1_on:
-            self.send('D', 1)
-        elif self.L1_on:
-            self.send('H', -1)
-        else:
-            self.send('H', -1)
-            self.send('D', -1)
+        self.send('A3', 1)
+
 
 
 #     def on_L3_y_at_rest(self):
