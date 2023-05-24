d08faf53fd7769b31224d97fef40fdd90986d833
Popov Alex <Alex-cvc@mail.ru>
2023-05-24 20:49:01 +0500

    Joy fix

--- raspberry/joystick.py
+++ raspberry/joystick.py
@@ -2,23 +2,21 @@ from pyPS4Controller.controller import Controller
 import serial
 from time import sleep
 
+
 class MyController(Controller):
     def __init__(self, **kwargs):
         super().__init__(self, **kwargs)
         self.L1_on = False
         self.R1_on = False
-        self.ser = serial.Serial("/dev/ttyACM0", 57600)
-
+        self.ser = serial.Serial("/dev/serial0", 115200)
 
         self.default_pos = {'A1': 0, 'A2': 0, 'A3': 0, 'A4': 0}
         self.current_pos = {'A1': 0, 'A2': 0, 'A3': 0, 'A4': 0}
         self.restrictions = {'A1': (-4096, 4096), 'A2': (-4096, 4096), 'A3': (-4096, 4096), 'A4': (-4096, 4096)}
 
-    def send(self, lit, sign, val = 100):
-        if lit[0] != 'A' or self.restrictions[lit][0] < self.current_pos[lit] + sign * val < self.restrictions[lit][1] :
-            self.ser.write((lit + str(sign * val).rjust(4, '0')).encode())
-            if lit[0] == 'A':
-                self.current_pos[lit] += sign * val
+    ##############################################
+    #                   Макросы                  #
+    ##############################################
 
     def on_L1_press(self):
         self.L1_on = True
@@ -32,32 +30,46 @@ class MyController(Controller):
     def on_R1_release(self):
         self.R1_on = False
 
-    def on_L2_press(self, value):
-        if self.L1_on: sign = -1
-        else: sign = 1
+    ##############################################
+    #             маршевые двигатели             #
+    ##############################################
 
-        # нужно вычислить коэффициент k
-        k = 1
-        self.send("M1", sign, value * k)
-        self.send("M3", sign, value * k)
+    def _motor_command_generator(self, index, value, reverse=False, stp=False):
+        if reverse:
+            return ("M" + str(index) + str(round(1000 + (value + 34000) / (34000 * 2) * 1000)).rjust(4, '0')).encode()
+        else:
+            return ("M" + str(index) + str(round((value + 34000) / (34000 * 2) * 1000)).rjust(4, '0')).encode()
 
-    def on_L2_release(self):
-        self.send("M1", 1, 0)
-        self.send("M3", 1, 0)
+        if stp:
+            return ("M" + str(index) + str(0).rjust(4, '0')).encode()
 
     def on_R2_press(self, value):
-        if self.L1_on: sign = -1
-        else: sign = 1
+        self.ser.write(self._motor_command_generator(1, value, self.R1_on))
+        self.ser.write(self._motor_command_generator(3, value, self.R1_on))
+        # self.ser.write(b'M01000')
 
-
-        # нужно вычислить коэффициент k
-        k = 1
-        self.send("M2", sign, value * k)
-        self.send("M4", sign, value * k)
+    def on_L2_press(self, value):
+        self.ser.write(self._motor_command_generator(2, value, self.L1_on))
+        self.ser.write(self._motor_command_generator(4, value, self.L1_on))
 
     def on_R2_release(self):
-        self.send("M2", 1, 0)
-        self.send("M4", 1, 0)
+        self.ser.write(self._motor_command_generator(1, 0, stp=True))
+        self.ser.write(self._motor_command_generator(3, 0, stp=True))
+        # self.ser.write(b'M01000')
+
+    def on_L2_release(self):
+        self.ser.write(self._motor_command_generator(2, 0, stp=True))
+        self.ser.write(self._motor_command_generator(4, 0, stp=True))
+
+    ##############################################
+    #                Сервоприводы                #
+    ##############################################
+
+    def send(self, lit, sign, val=100):
+        if lit[0] != 'A' or self.restrictions[lit][0] < self.current_pos[lit] + sign * val < self.restrictions[lit][1]:
+            self.ser.write((lit + str(sign * val).rjust(4, '0')).encode())
+            if lit[0] == 'A':
+                self.current_pos[lit] += sign * val
 
     def on_x_press(self):
         self.send('A2', 1)
@@ -84,32 +96,5 @@ class MyController(Controller):
         self.send('A3', 1)
 
 
-
-#     def on_L3_y_at_rest(self):
-#         pass
-#
-#     # self.ser.write(('A' + str(2048)).encode())
-#     def on_R3_y_at_rest(self):
-#         # self.ser.write(('B' + str(2048)).encode())
-#         pass
-#
-#     def on_L3_down(self, value):
-#         print(str(2048 - int(value / 16)).rjust(4, '0'))
-#         # self.ser.write(('A' + str(2048 - int(value/16)).rjust(4, '0')).encode())
-#
-#     def on_L3_up(self, value):
-#         print(str(2048 - int(value / 16)).rjust(4, '0'))
-#         # self.ser.write(('A' + str(2048 - int(value/16)).rjust(4, '0')).encode())
-#
-#     def on_R3_up(self, value):
-#         print(str(2048 - int(value / 16)).rjust(4, '0'))
-#         # self.ser.write(('B' + str(2048 - int(value / 16)).rjust(4, '0')).encode())
-#
-#     def on_R3_down(self, value):
-#         print(str(2048 - int(value / 16)).rjust(4, '0'))
-#         # self.ser.write(('B' + str(2048 - int(value / 16)).rjust(4, '0')).encode())
-
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.listen()
-
-    ## https://github.com/ArturSpirin/pyPS4Controller
+controller.listen(timeout=100)
